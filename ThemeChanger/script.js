5aa4bd01d9298116d70f8fa9c4b7439083ca8c12
Neeraj Jangra <jangraneeraj30@gmail.com>
2022-07-27 16:47:15 +0530

    ThemeChanger

---
+++ ThemeChanger/script.js
@@ -0,0 +1,27 @@
+function blackTheme() {
+  document.getElementById("container").style.backgroundColor =
+    "rgb(29, 29, 29)";
+  document.getElementById("textarea").style.backgroundColor = "rgb(29, 29, 29)";
+  document.getElementById("textarea").style.color = "white";
+  document.getElementById("textarea").style.borderColor = "white";
+}
+function blueTheme() {
+  document.getElementById("container").style.backgroundColor = "blue";
+  document.getElementById("textarea").style.backgroundColor = "blue";
+  document.getElementById("textarea").style.color = "white";
+  document.getElementById("textarea").style.borderColor = "white";
+}
+
+function yellowTheme() {
+  document.getElementById("container").style.backgroundColor = "yellow";
+  document.getElementById("textarea").style.backgroundColor = "yellow";
+  document.getElementById("textarea").style.color = "black";
+  document.getElementById("textarea").style.borderColor = "black";
+}
+
+function greenTheme() {
+  document.getElementById("container").style.backgroundColor = "green";
+  document.getElementById("textarea").style.backgroundColor = "green";
+  document.getElementById("textarea").style.color = "black";
+  document.getElementById("textarea").style.borderColor = "black";
+}
